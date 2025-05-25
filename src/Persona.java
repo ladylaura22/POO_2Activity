274ea2ca8a8cac51042f9c40d1abd819c7720f8d
ladylaura22 <lady.olmos@est.iudigital.edu.co>
2025-05-25 00:27:50 -0500

    lUEGO DE CONVERTIR A METODOS: Hemos organizado el código de manera más eficiente. se corrigieron como 10 errores de sintaxis, se cierra el escaner y se creó el método para contar el genero buscado.

--- src/Persona.java
+++ src/Persona.java
@@ -16,6 +16,7 @@ public class Persona {
         this.edad = edad;
 
     }
+
     // **Constructor adicional solo con nombre y género**
     public Persona(String nombre, String genero) {
         this.nombre = nombre;
@@ -42,10 +43,6 @@ public class Persona {
     }
 
 
-
-
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Persona> personas = new ArrayList<>();
@@ -66,79 +63,73 @@ public class Persona {
 
             personas.add(new Persona(nombre, apellido, genero, edad));
 
-            System.out.Println("Datos de todas las personas ingresadas:" + mostrarPersonas(personas));
-            System.out.Println("Ahora el método CopiarNombresYGeneros se encargará de copiar los nombres y géneros de las personas ingresadas.");
-            System.out.println(" Cantidad de personas masculinas: " + contarGenero(personas, "M"));
-            System.out.println(" Cantidad de personas femeninas: " + contarGenero(personas, "F"));
-
 
         }
 
-        // METODO PARA MOSTRAR tODO
 
-        public static void mostrarPersonas(List<Persona> personas) {
-            System.out.println("Lista de personas ingresadas con todos sus datos:");
-            for (Persona persona : personas) {
-                System.out.println("Nombre: " + persona.getNombre() + "\nApellido: " + persona.getApellido() +
-                        "\nGénero: " + persona.getGenero() + "\nEdad: " + persona.getEdad());
+        mostrarPersonas(personas);
+        List<Persona> nombresYGeneros = copiarNombresYGeneros(personas);
+        imprimirNombresYGeneros(nombresYGeneros);
 
-            }
-
-
-
-        // METODO PARA TOMAR NOMNBRES Y GENEROS
-        Public static void CopiarNombresYGeneros(List<Persona> personas) {
-        List<Persona> NombresYGeneros = new ArrayList<>();
-
-        for (Persona persona : personas) {
-            NombresYGeneros.add(new Persona(persona.getNombre(), persona.getGenero()));
-
-        }
-
-        // IMPRIMIR NOMBRES Y GENEROS
-        Public static void ImprimirNombresYGeneros(List<Persona> NombresYGeneros) {
-                    System.out.println("--------------------------------------");
-                    System.out.println("LISTA DE NOMBRES Y GÉNEROS:");
-                    System.out.println("Nombres y géneros capturados:");
-                    int i = 1; // Contador para enumerar los nombres
-                    for (Persona persona : NombresYGeneros) {
-                        System.out.println("Nombre " + (i++) + ": " + persona.getNombre()
-                                + " Genero de " + persona.getNombre() + ": " + persona.getGenero());
-                    }
-                }
-
-
-
-
-        // METODO PARA CALCULAR EL PROMEDIO DE EDADES
-        double suma = 0;
-
-        for (Persona persona : personas) {
-            suma += persona.getEdad();
-        }
-        double promedio = (double) suma / personas.size();
-        System.out.print("PROMEDIO DE EDADES: " + promedio);
-
-
-        // Método para contar personas según género
-        public static int contarGenero(ArrayList<Persona> personas, String generoBuscado) {
-            int contador = 0;
-            for (Persona p : personas) {
-                if (p.getGenero().equalsIgnoreCase(generoBuscado)) {
-                    contador++;
-                }
-            }
-            return contador;
-
-
-
-
+        System.out.println("Cantidad de personas masculinas: " + contarGenero(personas, "Masculino"));
+        System.out.println("Cantidad de personas femeninas: " + contarGenero(personas, "Femenino"));
+        System.out.println("PROMEDIO DE EDADES: " + calcularPromedioEdad(personas));
 
         scanner.close();
     }
+
+    // METODO PARA MOSTRAR tODO
+    public static void mostrarPersonas(List<Persona> personas) {
+        System.out.println("Lista de personas ingresadas con todos sus datos:");
+        for (Persona persona : personas) {
+            System.out.println("Nombre: " + persona.getNombre() + "\nApellido: " + persona.getApellido() +
+                    "\nGénero: " + persona.getGenero() + "\nEdad: " + persona.getEdad());
+
+        }
+    }
+
+
+    // METODO PARA TOMAR NOMNBRES Y GENEROS
+    public static List<Persona> copiarNombresYGeneros(List<Persona> personas) {
+        List<Persona> nombresYGeneros = new ArrayList<>();
+        for (Persona persona : personas) {
+            nombresYGeneros.add(new Persona(persona.getNombre(), persona.getGenero()));
+        }
+        return nombresYGeneros;
+    }
+
+    // IMPRIMIR NOMBRES Y GENEROS
+    public static void imprimirNombresYGeneros(List<Persona> nombresYGeneros) {
+        System.out.println("--------------------------------------");
+        System.out.println("LISTA DE NOMBRES Y GÉNEROS:");
+        System.out.println("Nombres y géneros capturados:");
+        int i = 1; // Contador para enumerar los nombres
+        for (Persona persona : nombresYGeneros) {
+            System.out.println("Nombre persona #" + (i++) + ": " + persona.getNombre()
+                    + "\nGenero de " + persona.getNombre() + ": " + persona.getGenero());
+        }
+    }
+
+
+    // METODO PARA CALCULAR EL PROMEDIO DE EDADES
+    public static double calcularPromedioEdad(List<Persona> personas) {
+        double suma = 0;
+        for (Persona persona : personas) {
+            suma += persona.getEdad();
+        }
+        double promedio = suma / personas.size();
+        return promedio;
+    }
+
+
+    // Método para contar personas según género
+    public static int contarGenero(List<Persona> personas, String generoBuscado) {
+        int contador = 0;
+        for (Persona p : personas) {
+            if (p.getGenero().equalsIgnoreCase(generoBuscado)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
 }
-
-
-
-
-
